30baa8b13392ebb90708390ee4f2c01a7b8a728f
Lucas Reda <lucasreda@gmail.com>
2018-04-04 17:42:31 -0300

    foi[

---
+++ app/controllers/homeController.js
@@ -0,0 +1,42 @@
+app.controller('homeController', function($scope) {
+
+    $scope.showAdicionar =false;
+    $scope.fecharBotao = false;
+    $scope.exibirBotao = true;
+    $scope.buttonAdd = function(){
+        $scope.showAdicionar = true;
+        $scope.fecharBotao = true;
+        $scope.exibirBotao = false;
+    }
+
+    $scope.fecharAdd = function(){
+        $scope.showAdicionar = false;
+        $scope.fecharBotao = false;
+        $scope.exibirBotao = true;
+    }
+
+	    $scope.item = {};
+        $scope.itens = [
+            {nome: 'Carlos Roberto', email: 'email@teste.com', mensagem: 'Lorem Ipsum' },
+            {nome: 'Ana Figueiredo', email: 'meumail@gmail.com', mensagem: 'Lorem Ipsum'}
+        ];
+    
+        $scope.adicionaItem = function () {
+            
+            
+
+            $scope.itens.push({nome: $scope.item.nome, email: $scope.item.email, mensagem: $scope.item.mensagem});
+            $scope.item.nome = $scope.item.email = '';
+            $scope.fecharAdd();
+
+            toastr.success("Item adicionado com sucesso.");
+        };
+    
+    
+        $scope.deleteItem = function(index){
+            $scope.itens.splice(index, 1);
+            toastr.success("Item removido com sucesso.");
+           
+        };
+
+});
